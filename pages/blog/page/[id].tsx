83ba2fdea8d0f9fcb7ebd5ab063a544833921b69
takehiro <takehiro-r133@hotmail.co.jp>
2022-04-22 17:55:53 +0900

    add Pagination component #25

--- pages/blog/page/[id].tsx
+++ pages/blog/page/[id].tsx
@@ -1,9 +1,11 @@
 import { NextPage } from "next"
 import { useEffect } from "react"
-import { ArticleList } from "../components/ArticleList"
-import { Layout } from "../components/Layout"
-import { client } from "../libs/client"
-import { Pagination } from "../components/Pagination"
+import { ArticleList } from "../../../components/ArticleList"
+import { Layout } from "../../../components/Layout"
+import { client } from "../../../libs/client"
+import { Pagination } from "../../../components/Pagination"
+
+const PER_PAGE = 5
 
 type Props = {
   blog: Blog[]
@@ -24,6 +26,21 @@ const Home: NextPage<Props> = ({ blog, tags, totalCount }) => {
 }
 
 export default Home
+
+export const getStaticPaths = async () => {
+  const repos = await client.get({ endpoint: "blog" })
+
+  const pageNumbers = []
+
+  const range = (start: number, end: number) =>
+    [...Array(end - start + 1)].map((_, i) => start + i)
+
+  const paths = range(1, Math.ceil(repos.totalCount / PER_PAGE)).map(
+    (repo) => `/blog/page/${repo}`
+  )
+
+  return { paths, fallback: false }
+}
 
 // データをテンプレートに受け渡す部分の処理
 export const getStaticProps = async () => {
