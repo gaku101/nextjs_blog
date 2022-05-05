e0dea97d90b17f02c831386d623441e8cd476bde
takehiro <takehiro-r133@hotmail.co.jp>
2022-05-05 21:19:49 +0900

    add style #25

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -6,20 +6,18 @@ type Props = {
 }
 
 export const Pagination: React.VFC<Props> = ({ totalCount }) => {
-  const PER_PAGE = 5
+  const PER_PAGE = 1
 
   const range = (start: number, end: number) =>
     [...Array(end - start + 1)].map((_, i) => start + i)
 
   return (
-    <ul>
+    <div className='flex justify-center'>
       {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
-        <li key={index}>
-          <Link href={`/blog/page/${number}`}>
-            <a>{number}</a>
-          </Link>
-        </li>
+        <Link href={`/blog/page/${number}`} key={index}>
+          <a className='px-4 py-2 m-2 rounded-md border border-cyan-700 text-cyan-700'>{number}</a>
+        </Link>
       ))}
-    </ul>
+    </div>
   )
 }
