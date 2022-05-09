66f47dbdcdee6c5f8051f3710ee34bbaa0fc27c1
takehiro <takehiro-r133@hotmail.co.jp>
2022-05-09 20:45:27 +0900

    small fix #27

--- pages/tag/[tag]/page/[id].tsx
+++ pages/tag/[tag]/page/[id].tsx
@@ -20,7 +20,7 @@ const TagId: NextPage<Props> = ({ tagId, blog, tags, totalCount }) => {
     <Layout tags={tags} searchedBy={tagId}>
       <ArticleList articles={blog} />
       <Pagination
-        totalCount={2}
+        totalCount={totalCount}
         currentPage={Number(id)}
         url={`/tag/${tag}/page`}
       />
